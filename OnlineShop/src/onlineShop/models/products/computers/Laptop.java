d49efa344d22b60435df440b5abe72df5365eae8
CHAN <chaneto800ss@gmail.com>
2021-04-08 17:54:26 +0300

    add file

--- OnlineShop/src/onlineShop/models/products/computers/Laptop.java
+++
@@ -1,8 +0,0 @@
-package onlineShop.models.products.computers;
-
-public class Laptop extends BaseComputer{
-
-    public Laptop(int id, String manufacturer, String model, double price) {
-        super (id, manufacturer, model, price, 10);
-    }
-}
