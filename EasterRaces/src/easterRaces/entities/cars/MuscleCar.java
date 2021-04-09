16d2e7e1155f656494477bb1a3c8ad553cf0fda1
CHAN <chaneto800ss@gmail.com>
2021-04-09 07:06:25 +0300

    add file

--- EasterRaces/src/easterRaces/entities/cars/MuscleCar.java
+++
@@ -1,8 +0,0 @@
-package easterRaces.entities.cars;
-
-public class MuscleCar extends BaseCar{
-
-    public MuscleCar(String model, int horsePower) {
-        super (model, horsePower,5000);
-    }
-}
--- EasterRaces/src/easterRaces/entities/cars/SportsCar.java
+++
@@ -1,8 +0,0 @@
-package easterRaces.entities.cars;
-
-public class SportsCar extends BaseCar{
-
-    public SportsCar(String model, int horsePower) {
-        super (model, horsePower, 3000);
-    }
-}
